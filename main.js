7a6e26eb472f1a5f73cdd8aaf465faf1bd5493c5
aishu1209 <89282363+aishu1209@users.noreply.github.com>
2022-12-04 11:45:05 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 song_1 = "";
 song_2 = "";
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload(){
     song_1 = loadSound("meera.mp3");
     song_2 = loadSound("drivers_license.mp3");
@@ -11,10 +14,32 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
     
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
     
+}
+
+function modelLoaded(){
+    console.log("model is loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("left wrist X =" + leftWristX + "and left wrist y =" + leftWristY);
+        console.log("right wrist X " + rightWristX + "and right wrist y = " + rightWristY);
+    }
 }
