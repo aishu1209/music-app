85c571258d45162a4160eed924439cf7f84f80ac
aishu1209 <89282363+aishu1209@users.noreply.github.com>
2023-01-11 16:43:44 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 leftWristScore = 0;
+rightWristscore = 0;
 status_meera = "";
 status_so_good = "";
 function preload(){
-    song_1 = loadSound("meera.mp3");
-    song_2 = loadSound("so_good.mp3");
+    song_2 = loadSound("meera.mp3");
+    song_1 = loadSound("so_good.mp3");
 }
 
 function setup(){
@@ -33,13 +34,22 @@ function draw(){
     stroke('#FF0000');
 
     status_meera = song_1.isPlaying();
+    status_so_good = song_2.isPlaying();
 
     if(leftWristScore > 0.2){
         circle(leftWristX, leftWristY, 20);
         song_2.stop();
-        if(status_meera = "false"){
+        if(status_so_good = "false"){
             song_1.play();
-            document.getElementById("heading").innerHTML = "Meera - Raja Kumari";
+            document.getElementById("song").innerHTML = "So Good - Weston Estate";
+        }
+        if(rightWristScore > 0.2){
+            circle(rightWristX, rightWristY, 20);
+            song_1.stop();
+            if(status_meera = "false"){
+                song_2.play();
+                document.getElementById("song").innerHTML = "Meera - Raja Kumari";
+            }
         }
     }
 
@@ -54,6 +64,7 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         console.log("score left wrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
